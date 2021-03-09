fdf56622e858ac828819660ce3de18152512527e
Alexander Rothman <gnomesort@megate.ch>
2021-03-09 13:52:34 -0800

    Improve main program.
    Add a decent getopt interface to main.c.
    Add decent error messages.

--- src/main.c
+++ src/main.c
@@ -2,29 +2,90 @@
 #include <inttypes.h>
 #include <limits.h>
 
+#include <getopt.h>
+
 #include "perfect.h"
 
-int main() {
+#define HELP \
+  "Usage: %s [options]\n" \
+  "Options:\n" \
+  "  --help, -h\n" \
+  "    Print this help message.\n" \
+  "  --bits=<8,16,32,64>, -b=<8,16,32,64>\n" \
+  "    Generate perfect numbers within the range of the given bit width."
+
+static const struct option longopts[] = {
+  { "bits", required_argument, NULL, 'b' },
+  { "help", no_argument, NULL, 'h' },
+  { NULL, 0, NULL, 0 }
+};
+
+int main(int argc, char** argv) {
+  size_t bits = sizeof(void*) * CHAR_BIT; // This is probably the processor word length.
+  {
+    int opt = 0;
+    while ((opt = getopt_long(argc, argv, "hb:", longopts, NULL)) != -1)
+    {
+      switch (opt)
+      {
+      case 'b':
+        sscanf(optarg, "%zu\n", &bits);
+        break;
+      case 'h':
+        printf(HELP "\n", argv[0]);
+        goto done;
+      default:
+        break;
+      }
+    }
+  }
+
   enum perfect_result res = PERFECT_RESULT_SUCCESS;
   struct perfect_generator* generator = NULL;
   res = perfect_create_generator(&generator);
   if (res != PERFECT_RESULT_SUCCESS)
   {
+    fprintf(stderr, "Failed to create perfect_generator.\n");
     goto err;
   }
-  uint32_t perfect = 0;
-  while ((res = perfect_next32(generator, &perfect)) != PERFECT_RESULT_NOMORE)
-  {
-    if (res < 0)
+  union {
+    uint8_t u8;
+    uint16_t u16;
+    uint32_t u32;
+    uint64_t u64;
+  } value;
+  do {
+    switch (bits)
     {
+    case 8:
+      res = perfect_next8(generator, &value.u8);
+      break;
+    case 16:
+      res = perfect_next16(generator, &value.u16);
+      break;
+    case 32:
+      res = perfect_next32(generator, &value.u32);
+      break;
+    case 64:
+      res = perfect_next64(generator, &value.u64);
+      break;
+    default:
+      fprintf(stderr, "Bad bit width.\n");
       goto err;
     }
-    printf("%" PRIu32 "\n", perfect);
-  }
+    if (res < 0)
+    {
+      fprintf(stderr, "Failed to generate perfect numbers.\n");
+      goto err;
+    }
+    if (res == PERFECT_RESULT_SUCCESS)
+    {
+      printf("%" PRIu64 "\n", value.u64);
+    }
+  } while (res != PERFECT_RESULT_NOMORE);
   perfect_destroy_generator(&generator);
   goto done;
 err:
-  fprintf(stderr, "Failed to generate perfect numbers.");
   return 1;
 done:
   return 0;
